99fa3ce2441189c41fa30b273626aec8adee0f1a
Grove <grove1411@gmail.com>
2021-12-25 22:13:02 -0200

    Created files functions

---
+++ functions/files.py
@@ -0,0 +1,7 @@
+def saveSheet(path, toPath):
+    # This will copy the actual excel sheet and save it in other path selected
+    pass
+
+def importSheet(path, toPath="./"):
+    # This will copy the selected excel sheet and save it in the actual path
+    pass
